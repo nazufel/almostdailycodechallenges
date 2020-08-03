b72773967cc9e216b7676324ffc5c0ab91c5338f
ryanthebossross <ryanthebossross@gmail.com>
2020-08-03 10:24:26 -0400

    moved back to a single func

--- src/00-fizzbuzz/main.go
+++ src/00-fizzbuzz/main.go
@@ -8,34 +8,21 @@ const b = 5
 func main() {
 
 	for i := 0; i <= 100; i++ {
-		r := fizzBuzz(i)
-		if len(r) != 0 {
-			fmt.Println(r)
+		// if i is devisable by f
+		if i%f == 0 && i%b != 0 {
+			fmt.Println("fizz")
 		}
-		if len(r) == 0 {
+		// if i is devisable by b
+		if i%b == 0 && i%f != 0 {
+			fmt.Println("buzz")
+		}
+		// if i is devisable by both f and b
+		if i%f == 0 && i%b == 0 {
+			fmt.Println("fizzbuzz")
+		}
+		// if i is devisable by both f and b
+		if i%f != 0 && i%b != 0 {
 			fmt.Println(i)
 		}
-
 	}
-
-}
-
-func fizzBuzz(c int) string {
-
-	var res string
-
-	// if i is devisable by f
-	if c%f == 0 && c%b != 0 {
-		res = "fizz"
-	}
-	// if i is devisable by b
-	if c%b == 0 && c%f != 0 {
-		res = "buzz"
-	}
-	// if i is devisable by both f and b
-	if c%f == 0 && c%b == 0 {
-		res = "fizzbuzz"
-	}
-
-	return res
 }
